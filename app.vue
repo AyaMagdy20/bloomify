1e48819d8a08da9df68de13e8789d43517e59d37
aya <ayamagdy.200a@gmail.com>
2024-07-01 14:33:49 +0300

    feat: add tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <h2 class="font-bold text-pink-800">Helloo</h2>
+    <span
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ea
+      autem, quo harum exercitationem velit distinctio unde rem animi quibusdam
+      ullam minus! Rerum explicabo, nihil debitis veniam vel quia? Dolorem modi
+      inventore quibusdam numquam iusto facere facilis sapiente tenetur, aliquam
+      asperiores excepturi incidunt culpa, quo voluptatem! Enim quaerat fuga
+      distinctio.</span
+    >
   </div>
 </template>
